b4849bfc1f14976752ed9e925b3842a27eecd549
21kd1a04g6 <147579944+21kd1a04g6@users.noreply.github.com>
2024-06-29 11:12:42 +0530

    Sumof3NumbersandtheirSum.java

---
+++ java codes/Sumof3NumbersandtheirSum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class sumoftwonumsevenodd
+{
+    public static void main(String[] args){
+    int a,b,c,sum;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter the a value");
+    a=sc.nextInt();
+    System.out.println("enter the b value");
+    b=sc.nextInt();
+    System.out.println("enter the c value");
+    c=sc.nextInt();
+    sum=a+b+c;
+    System.out.println(sum);
+    }
+}
